e0277c57ad99e17686965d37b80de45dbc5be879
Bálint Bertalan <blintber@gmail.com>
2020-09-16 20:14:20 +0200

    Initial android commit

---
+++ Android/app/src/main/java/com/itsecurityteam/caffstore/view/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.itsecurityteam.caffstore.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.itsecurityteam.caffstore.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
