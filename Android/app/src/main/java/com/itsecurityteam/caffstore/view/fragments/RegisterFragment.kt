205148f5598fffbfddba1e7d594d523df5d475b4
Bálint Bertalan <blintber@gmail.com>
2020-09-16 20:25:57 +0200

    Clean up register fragment (+ strings.xml)

--- Android/app/src/main/java/com/itsecurityteam/caffstore/view/fragments/RegisterFragment.kt
+++ Android/app/src/main/java/com/itsecurityteam/caffstore/view/fragments/RegisterFragment.kt
@@ -9,13 +9,11 @@ import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.itsecurityteam.caffstore.R
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class RegisterFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
+            inflater: LayoutInflater,
+            container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
@@ -25,8 +23,5 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 }
