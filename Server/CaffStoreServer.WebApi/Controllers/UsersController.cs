6bb82148e06c4ecde23a420c52d6f9ea4ddeb16e
Papp Kristóf Szabolcs <pappkristof3@gmail.com>
2020-09-28 17:54:50 +0200

    Implement dummy UsersController

--- Server/CaffStoreServer.WebApi/Controllers/UsersController.cs
+++ Server/CaffStoreServer.WebApi/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CaffStoreServer.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CaffStoreServer.WebApi.Controllers
 {
@@ -7,8 +8,33 @@ namespace CaffStoreServer.WebApi.Controllers
     public class UsersController : ControllerBase
     {
         public UsersController()
-        {
-            
+        { 
+
         }
+
+        [HttpPut]
+        public ActionResult<LoginResponse> Login([FromBody] LoginRequest request)
+        {
+            if(request.Username == "admin")
+            {
+                var response = new LoginResponse
+                {
+                    IsSuccess = true,
+                    Token = "asdfgh",
+                    UserId = 12,
+                    UserType = UserType.Admin
+                };
+
+                return Ok(response);
+            }
+
+            if (request.Username == "me")
+            {
+                return Unauthorized();
+            }
+
+            return NotFound();
+        }
+        
     }
 }
