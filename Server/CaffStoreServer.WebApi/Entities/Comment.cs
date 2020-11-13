9b411341a20768e0716b5e94d2041a70a3a81873
Papp Kristóf Szabolcs <pappkristof3@gmail.com>
2020-11-13 11:13:07 +0100

    Comment entity

---
+++ Server/CaffStoreServer.WebApi/Entities/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace CaffStoreServer.WebApi.Entities
+{
+    public class Comment
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string AddTime { get; set; }
+        public string Text { get; set; }
+    }
+}
