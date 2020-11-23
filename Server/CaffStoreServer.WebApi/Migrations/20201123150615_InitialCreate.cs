f802db3211b5963f106411345e3fc32396d21879
Papp Kristóf Szabolcs <pappkristof3@gmail.com>
2020-11-23 16:16:36 +0100

    DB migrations

---
+++ Server/CaffStoreServer.WebApi/Migrations/20201123150615_InitialCreate.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CaffStoreServer.WebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Caffs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    CreationDate = table.Column<string>(nullable: true),
+                    Creator = table.Column<string>(nullable: true),
+                    Duration = table.Column<int>(nullable: false),
+                    ThumbnailUrl = table.Column<string>(nullable: true),
+                    Cost = table.Column<double>(nullable: false),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Caffs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CaffId = table.Column<long>(nullable: false),
+                    UserName = table.Column<string>(nullable: true),
+                    AddTime = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comment_Caffs_CaffId",
+                        column: x => x.CaffId,
+                        principalTable: "Caffs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "ConcurrencyStamp",
+                value: "f457dfaf-25c4-4c8d-a5cf-f00d677142cb");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "ConcurrencyStamp",
+                value: "9e29cc8a-8178-4632-b4c0-4b9c7552e8e8");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_CaffId",
+                table: "Comment",
+                column: "CaffId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comment");
+
+            migrationBuilder.DropTable(
+                name: "Caffs");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "ConcurrencyStamp",
+                value: "b776197b-87d5-44bf-8020-4cd4054a1ebc");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "ConcurrencyStamp",
+                value: "345bfff7-a73f-415b-8587-1f5558cb094a");
+        }
+    }
+}
