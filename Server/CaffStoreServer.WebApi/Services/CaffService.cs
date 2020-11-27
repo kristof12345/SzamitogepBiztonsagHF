6f02339f5750d48b71455c65522a2f2b9d997d45
bargergo <bargergo@gmail.com>
2020-11-27 19:22:11 +0100

    Added validation for file extension, size

--- Server/CaffStoreServer.WebApi/Services/CaffService.cs
+++ Server/CaffStoreServer.WebApi/Services/CaffService.cs
@@ -1,6 +1,7 @@
 ﻿using CaffStoreServer.WebApi.Context;
 using CaffStoreServer.WebApi.Entities;
 using CaffStoreServer.WebApi.Interfaces;
+using CaffStoreServer.WebApi.Models.Exceptions;
 using CaffStoreServer.WebApi.Models.Requests;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -14,6 +15,9 @@ namespace CaffStoreServer.WebApi.Services
     public class CaffService : ICaffService
     {
         private CaffStoreDbContext _context;
+
+        private string[] permittedExtensions = { ".caff" };
+        private readonly int maxFileSize = 32 * 1024 * 1024;
 
         public CaffService(CaffStoreDbContext context)
         {
@@ -59,6 +63,13 @@ namespace CaffStoreServer.WebApi.Services
 
         public Task Upload(string userId, UploadCAFFRequest request)
         {
+            var ext = Path.GetExtension(request.Image.FileName).ToLowerInvariant();
+
+            if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+                throw new BadRequestException("Not supported file extension");
+            if (request.Image.Length > maxFileSize)
+                throw new BadRequestException("Size limit reached");
+
             //TODO: Save file, call create()
             throw new NotImplementedException();
         }
