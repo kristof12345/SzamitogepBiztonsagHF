9f6c6a4bf8d34349c603bac9636aabbff1c0ee1a
Papp Kristóf Szabolcs <pappkristof3@gmail.com>
2020-11-28 14:11:47 +0100

    Fix test and implementation for bad password

--- Server/CaffStoreServer.WebApi/Controllers/UsersController.cs
+++ Server/CaffStoreServer.WebApi/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using CaffStoreServer.WebApi.Models.Requests;
 using CaffStoreServer.WebApi.Models.Responses;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -35,8 +36,15 @@ namespace CaffStoreServer.WebApi.Controllers
         [HttpPost]
         public async Task<ActionResult<RegisterResponse>> Register([FromBody] RegisterRequest request)
         {
-            var result = await _userService.CreateUserAsync(request);
-            return Ok(result);
+            try
+            {
+                var result = await _userService.CreateUserAsync(request);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return Conflict(e.Message);
+            }
         }
 
         [Authorize]
