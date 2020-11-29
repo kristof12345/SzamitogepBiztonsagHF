0012ae3fed9bb850c100236537ef3c9e7a6a6411
Somi47 <somorjai.balazs@danubisoft.hu>
2020-11-29 10:05:38 +0100

    CaffStoreServer file logging added
    Signed-off-by: Somi47 <somorjai.balazs@danubisoft.hu>

--- Server/CaffStoreServer.WebApi/Program.cs
+++ Server/CaffStoreServer.WebApi/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NetEscapades.Extensions.Logging.RollingFile;
 
 namespace CaffStoreServer.WebApi
 {
@@ -20,6 +22,19 @@ namespace CaffStoreServer.WebApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                    logging.AddFile(options =>
+                    {
+                        options.FileName = "CaffStoreServer-";
+                        options.LogDirectory = "LogFiles";
+                        options.FileSizeLimit = 20 * 1024 * 1024; // (20MB)
+                        options.Extension = "txt";
+                        options.Periodicity = PeriodicityOptions.Daily;
+                    });
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
